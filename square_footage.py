1996095f01ba6eced665c819f80d894b517d10a1
kristyannlong <144977219+kristyannlong@users.noreply.github.com>
2023-12-11 16:04:06 -0700

    Add files via upload

---
+++ square_footage.py
@@ -0,0 +1,21 @@
+# 1) Has a function to calculate the square footage of a house
+
+def calculate_square_footage(length, width):
+    """Calculate square footage."""
+    square_footage = length * width
+    return square_footage
+
+
+
+
+# 2) Has a function to calculate the circumference of a circle
+
+import math
+
+def calculate_circumference(radius):
+    """Calculate the circumference of a circle."""
+    circumference = 2 * math.pi * radius
+    return circumference
+
+
+
